b3a1d0b45fb1bd3a9c7b36455c157b1b7b8aa942
Travless <travis.white0101@gmail.com>
2023-01-29 10:53:35 -0500

    Construct knightMoves function that takes in the starting node's (knight's) location as well as its ending destination location

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const board = document.createElement('div');
 
 const possibleMoves = [[-1, 2], [-2, 1], [-1, -2], [-2, -1], [1, 2], [2, 1], [1, -2], [2, -1]];
 
-
+// Knight class to store knight's current location (via its row and column), as well as its destination coordinates
 class Knight {
     constructor(row, col, destCoor){
         this.row = row;
@@ -16,6 +16,7 @@ class Knight {
     }
 }
 
+// Generates traditional 8x8 chess board to visually represent knight's moves and provides user a simple "point and click" UI
 const chessBoard = (() => {
     for (let i = 0; i < 8; i++){
         let boardRow = document.createElement('div');
@@ -34,15 +35,32 @@ const chessBoard = (() => {
     }
 })
 
+// Create function that takes in the knight's current location and it's destination coordinates, and uses breadth first traversal (level order) to scan the nodes created for the shortest path to the destination
+function knightMoves (knightLocation, knightDestination) {
+    // Create queue to house nodes that are processed in a FIFO fashion
+    const queue = [];
+    // Generate starting node (knight) with "center" coordinates and no destination yet
+    const knight = new Knight(3, 3, 0);
+    // Push knight into queue
+    queue.push(knight);
+
+    // Create new Set to house visited nodes to avoid infinite cycles
+    const visited = new Set();
+
+    // While loop tracks length of queue and continues as long as its length is greater than 0
+    while(queue.length > 0){
+        
+    }
+}
+// At start of app, call chessBoard function to generate board, then use Knight class to create a new knight at the "center" area of chessboard.
+// will impliment user choice of starting location through newer iteration
 chessBoard();
-const knight = new Knight(3, 3);
-let knightRow = document.querySelector(`[row='${knight.row}']`);
-let knightCol = knightRow.childNodes[`${knight.col}`];
 
-console.log(knightRow);
-console.log(knightCol);
 
+// Add event listener to respond to user's click action to receive data to assign the target destination of the knight to it's created class.
 document.addEventListener('click', function(event){
     let destCol = event.target.getAttribute('column');
     let destRow = event.target.parentNode.getAttribute('row');
+    knight.destCoor = [destRow, destCol];
+    console.log(knight.destCoor);
 })
