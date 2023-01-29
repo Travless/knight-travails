d035cbf1124f991c77aba89f8a11e281b5c6bc9a
Travless <travis.white0101@gmail.com>
2023-01-29 11:05:06 -0500

    Change Knight class name to Node to avoid confusion when being used to generate other nodes, such as neighboring nodes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const board = document.createElement('div');
 const possibleMoves = [[-1, 2], [-2, 1], [-1, -2], [-2, -1], [1, 2], [2, 1], [1, -2], [2, -1]];
 
 // Knight class to store knight's current location (via its row and column), as well as its destination coordinates
-class Knight {
+class Node {
     constructor(row, col, destCoor){
         this.row = row;
         this.col = col;
@@ -35,12 +35,16 @@ const chessBoard = (() => {
     }
 })
 
+function getCurrentNeighbors(){
+
+}
+
 // Create function that takes in the knight's current location and it's destination coordinates, and uses breadth first traversal (level order) to scan the nodes created for the shortest path to the destination
 function knightMoves (knightLocation, knightDestination) {
     // Create queue to house nodes that are processed in a FIFO fashion
     const queue = [];
     // Generate starting node (knight) with "center" coordinates and no destination yet
-    const knight = new Knight(3, 3, 0);
+    const knight = new Node(3, 3, 0);
     // Push knight into queue
     queue.push(knight);
 
@@ -49,7 +53,24 @@ function knightMoves (knightLocation, knightDestination) {
 
     // While loop tracks length of queue and continues as long as its length is greater than 0
     while(queue.length > 0){
+        // Remove first node from queue
+        const current = queue.shift();
+        // Destructure properties of knight class to current node
+        const { row, col, destCoor } = current;
+
+        // Process node
         
+        // Add neighbors
+        for (const neighbpr of getCurrentNeighbors(row, col)){
+            // Use array destructuring to get neighbor coordinates
+            const [neighborRow, neighborCol] = neighbor;
+
+            // Check if neighboring node is outside of the board's boundaries
+            if (neighborRow < 0 || neighborCol < 0){
+                return;
+            }
+            if (visited.has()) continue;
+        }
     }
 }
 // At start of app, call chessBoard function to generate board, then use Knight class to create a new knight at the "center" area of chessboard.
