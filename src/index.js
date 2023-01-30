15740f47bf1e55027318011430c4fba03e837ab0
Travless <travis.white0101@gmail.com>
2023-01-30 11:32:26 -0500

    Clean up wording of logic. Remove out of bounds check to address bug fix halting process of knightMoves function

--- src/index.js
+++ src/index.js
@@ -5,21 +5,6 @@ const content = document.getElementById('content');
 
 const board = document.createElement('div');
 
-const possibleMoves = [[-1, 2], [-2, 1], [-1, -2], [-2, -1], [1, 2], [2, 1], [1, -2], [2, -1]];
-
-// Knight class to store knight's current location (via its row and column), as well as its destination coordinates
-class Node {
-    constructor(row, col, distance){
-        this.row = row;
-        this.col = col;
-        this.distance = distance;
-    }
-
-    getPositionToString(){
-        return `${this.row}, ${this.col}`;
-    }
-}
-
 // Generates traditional 8x8 chess board to visually represent knight's moves and provides user a simple "point and click" UI
 const chessBoard = (() => {
     for (let i = 0; i < 8; i++){
@@ -39,14 +24,30 @@ const chessBoard = (() => {
     }
 })
 
+
+const directions = [[-2,-1], [-2,1], [-1,2], [1,2], [2,1], [2,-1], [1,-2], [-1,-2]];
+
+// Knight class to store knight's current location (via its row and column), as well as its destination coordinates
+class Node {
+    constructor(row, col, distance){
+        this.row = row;
+        this.col = col;
+        this.distance = distance;
+    }
+
+    getPositionToString(){
+        return `${this.row}, ${this.col}`;
+    }
+}
+
 // will take the current row and col, and try to make a move in any of the possibleMoves available
-function getCurrentNeighbors(){
+const getCurrentNeighbors = (row, col) => {
     // Create empty array to house neighbors
     const neighbors = [];
 
     // loop through possibleMoves array assigning either coordinate (rowChange, colChange) to a direction
-    for (move of possibleMoves){
-        const [rowChange, colChange] = directions;
+    for (const direction of directions){
+        const [rowChange, colChange] = direction;
 
         // Assign neighborRow and neighborCol via the row/col in addition to their respective change amounts
         const neighborRow = row + rowChange;
@@ -54,19 +55,18 @@ function getCurrentNeighbors(){
 
         neighbors.push([neighborRow, neighborCol]);
     }
-
     // return list of neighbots
     return neighbors;
 }
 
 // Create function that takes in the knight's current location and it's destination coordinates, and uses breadth first traversal (level order) to scan the nodes created for the shortest path to the destination
-function knightMoves (destRow, destCol) {
+function knightMoves (destinationRow, destinationCol){
     // Create queue to house nodes that are processed in a FIFO fashion
     const queue = [];
     // Generate starting node (knight) with "center" coordinates and no destination yet
-    const knight = new Node(3, 3, 0);
+    const startNode = new Node (0, 0, 0);
     // Push knight into queue
-    queue.push(knight);
+    queue.push(startNode);
 
     // Create new Set to house visited nodes to avoid infinite cycles
     const visited = new Set();
@@ -74,47 +74,54 @@ function knightMoves (destRow, destCol) {
     // While loop tracks length of queue and continues as long as its length is greater than 0
     while(queue.length > 0){
         // Remove first node from queue
-        const current = queue.shift();
+        const node = queue.shift();
         // Destructure properties of knight class to current node
-        const { row, col, distance } = current;
+        const { row, col, distance } = node;
 
         // Process node
 
         // if the destination row and column are equivalent to the row and column of the knight, return the distance
-        if (row === destRow && col === destCol){
+        if(row === destinationRow && col === destinationCol){
             return distance;
         }
+        visited.add(node.getPositionToString());
 
-        visited.add(current.getPositionToString();)
-        
         // Add neighbors
-        for (const neighbor of getCurrentNeighbors(row, col)){
+
+        for(const neighbor of getCurrentNeighbors(row, col)) {
             // Use array destructuring to get neighbor coordinates
             const [neighborRow, neighborCol] = neighbor;
-            const currentNeighborNode = new Node(neighborRow, neighborCol, distance + 1);
+            const neighborNode = new Node(neighborRow, neighborCol, distance + 1);
 
-
-            // Check if neighboring node is outside of the board's boundaries
-            if (neighborRow < 0 || neighborCol < 0){
-                return;
-            }
             // Pass in Current Neighbor Node with getPositionToString method, that will return a specific string that identifies a node on its location
-            if (visited.has(currentNeighborNode.getPositionToString())) continue;
+            if(visited.has(neighborNode.getPositionToString())) continue;
 
             // push current neighbor onto queue to be processed
-            queue.push(currentNeighborNode);
+            queue.push(neighborNode);
         }
     }
 }
+
+console.log(knightMoves(3,4));
+
+
+
+
+
+
 // At start of app, call chessBoard function to generate board, then use Knight class to create a new knight at the "center" area of chessboard.
 // will impliment user choice of starting location through newer iteration
 chessBoard();
 
 
+
 // Add event listener to respond to user's click action to receive data to assign the target destination of the knight to it's created class.
 document.addEventListener('click', function(event){
-    let destCol = event.target.getAttribute('column');
-    let destRow = event.target.parentNode.getAttribute('row');
-    knight.destCoor = [destRow, destCol];
-    console.log(knight.destCoor);
+    // let destCol = event.target.getAttribute('column');
+    // let destRow = event.target.parentNode.getAttribute('row');
+    // console.log(destRow);
+    // console.log(destCol);
+    // knight.destCoor = [destRow, destCol];
+    // console.log(knight.destCoor);
+    // console.log(knightMoves(destRow, destCol));
 })
